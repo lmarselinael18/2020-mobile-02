45b834e67f575c0fdf515fe2c2a1f13d0779c16a
lmarselinael <lisamarselina183@gmail.com>
2020-02-12 20:57:36 +0700

    layout 1 2 3 done

---
+++ app/src/main/java/id/putraprima/marketplacelayout/Halaman_Login.java
@@ -0,0 +1,14 @@
+package id.putraprima.marketplacelayout;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Halaman_Login extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.halaman_login);
+    }
+}
---
+++ app/src/main/java/id/putraprima/marketplacelayout/Welcome.java
@@ -0,0 +1,15 @@
+package id.putraprima.marketplacelayout;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Welcome extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
